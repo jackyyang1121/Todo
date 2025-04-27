f06ac9c2372bff7519ca56adb9c31f523b4ef9f5
鄭名洋 <mingyongpig@gmail.com>
2025-04-27 18:55:01 +0000

    modify

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "your_app_db",
+        "USER": "your_app_user",
+        "PASSWORD": "your_app_password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 CORS_ALLOWED_ORIGINS = [
